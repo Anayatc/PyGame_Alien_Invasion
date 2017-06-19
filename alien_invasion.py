80ec9690f3092bccc80b75224d6453a0eccb69fc
Anayatc <anayatc@gmail.com>
2017-06-19 10:30:40 +0100

    created a settings module to house game settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,13 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     pygame.init()
     game_display = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (100, 100, 100)
 
     while True:
 
@@ -13,6 +15,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        game_display.fill(bg_color)
         pygame.display.update()
 
 run_game()
