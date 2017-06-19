be96c9d88e0b90c9caa8532b91677238e0eaf3e5
Anayatc <anayatc@gmail.com>
2017-06-19 11:19:37 +0100

    updated formatting for pep8

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,15 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    game_display = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    game_display = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(game_display)
 
     while True:
 
@@ -16,6 +18,7 @@ def run_game():
                 sys.exit()
 
         game_display.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.update()
 
 run_game()
--- ship.py
+++ ship.py
@@ -6,11 +6,11 @@ class Ship():
     def __init__(self, game_display):
         self.game_display = game_display
 
-        self.image = pygame.image.load('image/ship.png')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.game_display_rect = game_display.get_rect()
 
-        self.rect.center_x = self.game_display_rect.center_x
+        self.rect.centerx = self.game_display_rect.centerx
         self.rect.bottom = self.game_display_rect.bottom
 
     def blitme(self):
