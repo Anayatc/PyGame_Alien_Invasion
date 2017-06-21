2f215045212cdc55b09e2d969ff3a3f2e4a38441
Anayatc <anayatc@gmail.com>
2017-06-21 15:05:10 +0100

    changed background color

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ class Settings():
     def __init__(self):
         self.screen_width = 1200
         self.screen_height= 800
-        self.bg_color = (50, 50, 50)
+        self.bg_color = (15, 15, 15)
